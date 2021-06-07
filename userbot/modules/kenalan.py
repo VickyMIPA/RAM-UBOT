90e2a6871292e5cbf1597d0600dbccd7b7c2ae6c
VickyMIPA <83190867+VickyMIPA@users.noreply.github.com>
2021-06-07 23:32:21 +0700

    Update kenalan.py

--- userbot/modules/kenalan.py
+++ userbot/modules/kenalan.py
@@ -2,37 +2,37 @@ from time import sleep
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^.r(?: |$)(.*)')
+@register(outgoing=True, pattern='^.v(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("`Hai Perkenalkan Namaku Ramadhani`")
+    await typew.edit("`Hai Perkenalkan Nama Gue Vicky Gantenk 😃`")
     sleep(3)
-    await typew.edit("`19 Tahun`")
+    await typew.edit("`17 Tahun`")
     sleep(1)
-    await typew.edit("`Tinggal Di jakarta, Salam Kenal:)`")
+    await typew.edit("`Tinggal Di jakarta, Salken Ya ngentot!!!:)`")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^.sayang(?: |$)(.*)')
+@register(outgoing=True, pattern='^.rasya(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
-    await typew.edit("`Cuma Mau Bilang`")
+    await typew.edit("`Ganteng anet😇🤗`")
     sleep(3)
-    await typew.edit("`Aku Sayang Kamu`")
+    await typew.edit("`Imut banget :')`")
     sleep(1)
-    await typew.edit("`I LOVE YOU 💞`")
+    await typew.edit("`Intinya Aku sayang Kalean🙈😚`")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^.semangat(?: |$)(.*)')
+@register(outgoing=True, pattern='^.piki(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
-    await typew.edit("`Apapun Yang Terjadi`")
+    await typew.edit("`Anak nya baik,Rajin Me na bung, dan tidak sombong😁`")
     sleep(3)
-    await typew.edit("`Tetaplah Bernapas`")
+    await typew.edit("`Pokoknya Vicky tuh ganteng Bangettttt🏃🏃🏃`")
     sleep(1)
-    await typew.edit("`Dan Selalu Bersyukur`")
+    await typew.edit("`Dan Aku Bersyukur unchh😭👍🏻`")
 # Create by myself @localheart
